8b3505bbccb9f7ebe7d143df8f44de514e2b6410
Connotation <kkarrkid7@gmail.com>
2022-09-25 18:00:28 +0900

    [root] API Update

--- src/api/root.js
+++ src/api/root.js
@@ -4,11 +4,49 @@ const express = require('express');
 const router = express.Router();
 const path = require('path')
 const fs = require('fs')
+const Game = require('../models/game')
+
+router.get('/', (req, res) => {
+    res.json({success: false, msg: "Using Another Endpoint"})
+})
+
+// 테스트용 페이지
+
+router.get('/test', (req, res)=> {
+    res.render('test', {})
+});
 
 // 게임 시작
 
-router.post('/start', (req, res) => {
 
+router.post('/start', (req, res) => {
+    let response = {}
+    let gn = req.body.gameNumber
+    let gtc = req.body.gameTimerCnt
+    let gta = req.body.gameTimerAll
+    let gpc = req.body.gamePlayerCnt
+    let gcg = req.body.gameCategory
+
+    const newGame = new Game({
+        gameNumber: gn,
+        gameTimerCnt: gtc,
+        gameTimerAll: gta,
+        gamePlayerCnt: gpc,
+        gameCategory: gcg,
+        runningGame: true
+    });
+
+    newGame.save()
+        .then(p => {
+            console.log("== New Game ==")
+            console.log(p)
+            response = {code: 200, msg: "Game Start"}
+            res.json(response)
+        }).catch(e => {
+            console.log(e);
+            response = {code: 400, msg: "DB Error"}
+            res.json(response)
+        })
 });
 
 // + gameNumber, gameTimerCnt, gameTimerAll, gamePlayerCnt, gameCategory +
@@ -17,13 +55,61 @@ router.post('/start', (req, res) => {
 
 // 게임 종료
 
-router.get('/end', (req, res) => {
+router.get('/end/:gn', (req, res) => {
+    let response = {}
+    let gn = req.params.gn
 
+    if (isNaN(gn)){
+        response = {code: 400, msg: "Query Error"}
+        res.json(response)
+        return
+    }
+
+    console.log(gn)
+
+    Game.findOneAndDelete({gameNumber: gn*1})
+    .then(p => {
+        if(p) {
+            console.log("== End Game ==")
+            response = {code: 200, msg: "Game End"}
+            res.json(response)
+        } else {
+            console.log("== Not Found Game ==")
+            response = {code: 400, msg: "Not Found Game"}
+            res.json(response)
+        }
+    })
+    .catch(e => {
+        console.log(e);
+        response = {code: 400, msg: "DB Error"}
+        res.json(response)
+    })
 });
 
 // 게임 상태 조회
 
-router.get('/state', (req, res) => {
+router.get('/state/:gn', (req, res) => {
+    let response = {}
+    let gn = req.params.gn
+
+    if (isNaN(gn)){
+        response = {code: 400, msg: "Query Error"}
+        res.json(response)
+        return
+    }
+
+    console.log(gn)
+
+    Game.findOne({gameNumber: gn*1}, {_id: false, __v: false})
+    .then(p => {
+        response = p
+        res.json(response)
+    })
+    .catch(e => {
+        console.log(e)
+        response = {code: 400, msg:"DB Error"}
+        res.json(response)
+    })
 
 });
 
@@ -35,14 +121,66 @@ router.get('/setting', (req, res) => {
 
 // 게임 설정 초기화용
 
-router.post('/setting', (req, res) => {
+router.post('/setting/:gn', (req, res) => {
+    let response = {}
+    let gn = req.params.gn
 
+    if (isNaN(gn)){
+        response = {code: 400, msg: "Query Error"}
+        res.json(response)
+        return
+    }
+
+    const update = {}
+
+    let geb = req.body.gameEditBlind
+    if(geb) update['gameTimerCnt'] = geb
+
+    let get = req.body.gameEditTimer
+    if(get) update['gameTimerAll'] = get
+
+    let gep = req.body.gameEditPlayer
+    if(gep) update['gamePlayerCnt'] = gep
+
+    console.log(update)
+
+    Game.updateOne({gameNumber: gn*1}, update, { runValidators: true })
+    .then(p => {
+        response = p
+        res.json(response)
+    })
+    .catch(e => {
+        console.log(e)
+        response = {code: 400, msg: "DB Error"}
+        res.json(response)
+    })
+
+    
 });
 
 // 게임 설정 변경 2
 
-router.put('/setting', (req, res) => {
+router.put('/setting/:gn', (req, res) => {
+    let response = {}
+    let gn = req.params.gn
 
+    if (isNaN(gn)){
+        response = {code: 400, msg: "Query Error"}
+        res.json(response)
+        return
+    }
+
+    Game.findOne({gameNumber: gn*1}, {_id: false, __v: false})
+    .then(p => {
+        response = p
+        res.json(response)
+    })
+    .catch(e => {
+        console.log(e)
+        response = {code: 400, msg:"DB Error"}
+    })
 });
+
+// gameTimerCnt랑 gameEditBlind랑 같은거??
 
 module.exports = router;
--- src/models/game.js
+++ src/models/game.js
@@ -4,23 +4,28 @@ const mongoose = require('mongoose')
 
 const gameSchema = new mongoose.Schema({
     gameNumber: {
-        type: int,
-        required: true
+        type: Number,
+        required: true,
+        unique: true
     },
     gameTimerCnt: {
-        type: int,
+        type: Number,
         required: true
     },
     gameTimerAll: {
-        type: int,
+        type: Number,
         required: true
     },
     gamePlayerCnt: {
-        type: int,
+        type: Number,
         default: 0
     },
     gameCategory: {
-        type: int,
+        type: Number,
+        required: true
+    },
+    runningGame: {
+        type: Boolean,
         required: true
     }
 
@@ -29,34 +34,5 @@ const gameSchema = new mongoose.Schema({
 const Game = mongoose.model('Game', gameSchema)
 
 module.exports = Game
-
-const buySchema = new mongoose.Schema({
-    b_ref: {
-        type: String,
-        required: true
-    },
-    b_item: {
-        type: String,
-        required: true,
-        get: (data) => {
-            try {
-                return JSON.parse(data);
-            } catch (err) {
-                return data;
-            }
-        },
-        set: (data) => {
-            return JSON.stringify(data);
-        }
-    },
-    b_date: {
-        type: Date, 
-        default: Date.now
-    }
-})
-
-const Buy = mongoose.model('Buy', buySchema)
-
-module.exports = Buy
 
 
